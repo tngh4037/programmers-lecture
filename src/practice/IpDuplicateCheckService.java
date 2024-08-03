87475dc5131c709a15bd0fa2d7b3265ff05edfb0
tngh4037 <tngh4037@gmail.com>
2024-08-03 17:28:35 +0900

    feat: 파일을 이용한 IP별 이용 제한 로직 개발

---
+++ src/practice/IpDuplicateCheckService.java
@@ -0,0 +1,108 @@
+package practice;
+
+import java.io.*;
+import java.time.LocalDate;
+
+public class IpDuplicateCheckService {
+
+    private final int LIMIT_COUNT = 3;
+
+    public synchronized boolean checkIp(String ipAddress) {
+        BufferedReader br = null;
+
+        try {
+            br = new BufferedReader(new FileReader(createFile()));
+
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                String[] split = line.split(" ");
+                String ip = split[0];
+                int count = Integer.parseInt(split[1]);
+                if (ip.equals(ipAddress) && count >= LIMIT_COUNT) {
+                    return false;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+
+        return true;
+    }
+
+    public synchronized boolean saveIp(String ipAddress) {
+        String fileName = createFile();
+        BufferedReader br = null;
+        FileWriter bw = null;
+
+        StringBuilder sb = new StringBuilder();
+
+        try {
+            br = new BufferedReader(new FileReader(fileName));
+
+            String line = null;
+            boolean isExist = false;
+            while ((line = br.readLine()) != null) {
+                String[] split = line.split(" ");
+                String ip = split[0];
+                int count = Integer.parseInt(split[1]);
+
+                if (ip.equals(ipAddress)) {
+                    if (count >= LIMIT_COUNT) {
+                        return false;
+                    }
+
+                    isExist = true;
+                    count++;
+                }
+
+                sb.append(ip).append(" ").append(count).append("\n");
+            }
+
+            if (!isExist) {
+                sb.append(ipAddress).append(" ").append(1).append("\n");
+            }
+
+            sb.deleteCharAt(sb.length() - 1);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        } finally {
+           try {
+               br.close();
+           } catch (IOException e) {
+               throw new RuntimeException(e);
+           }
+        }
+
+        try {
+            bw = new FileWriter(fileName);
+            bw.write(sb.toString());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+             try {
+                 bw.close();
+             } catch (IOException e) {
+                 throw new RuntimeException(e);
+             }
+        }
+
+        return true;
+    }
+
+    private String createFile() {
+        String fileName = LocalDate.now() + ".txt";
+        File file = new File(fileName);
+        try {
+            if (file.createNewFile()) {
+                System.out.println(fileName + " created");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+
+        return fileName;
+    }
+}
---
+++ src/practice/IpDuplicateCheckServiceMain.java
@@ -0,0 +1,48 @@
+package practice;
+
+import java.util.List;
+
+public class IpDuplicateCheckServiceMain {
+
+    private static List<String> IP_LIST = List.of("127.0.0.1", "127.0.0.2", "127.0.0.3", "127.0.0.4", "127.0.0.5");
+
+    public static void main(String[] args) {
+        IpDuplicateCheckService ipDuplicateCheckService = new IpDuplicateCheckService();
+
+        Thread t1 = new Thread(new RunTask(ipDuplicateCheckService));
+        Thread t2 = new Thread(new RunTask(ipDuplicateCheckService));
+        Thread t3 = new Thread(new RunTask(ipDuplicateCheckService));
+        Thread t4 = new Thread(new RunTask(ipDuplicateCheckService));
+        Thread t5 = new Thread(new RunTask(ipDuplicateCheckService));
+
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+        t5.start();
+    }
+
+    static class RunTask implements Runnable {
+
+        private IpDuplicateCheckService ipDuplicateCheck;
+
+        public RunTask(IpDuplicateCheckService ipDuplicateCheck) {
+            this.ipDuplicateCheck = ipDuplicateCheck;
+        }
+
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(10);
+
+                for (int i = 0; i < IP_LIST.size(); i++) {
+                    boolean result = ipDuplicateCheck.saveIp(IP_LIST.get(i));
+                    System.out.println("[" + Thread.currentThread().getName() + "]" + " result: " + result + ", ip: " + IP_LIST.get(i));
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+}
